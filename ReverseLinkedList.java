7a434c0a7f2e4e67c2e5017382524b2d4440cc28
ratchet91 <39301777+ratchet91@users.noreply.github.com>
2024-05-03 14:05:25 +0530

    Add files via upload

---
+++ ReverseLinkedList.java
@@ -0,0 +1,18 @@
+
+public class ReverseLinkedList {
+	public class Node {
+		int val;
+		Node next;
+
+		Node(int val, Node next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
